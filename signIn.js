78551045b45028d0739cf9ef88ee7fce0b8aedcc
iamankit014 <ankitsingh4212@gmail.com>
2022-12-17 22:18:42 +0530

    day-5 work done

--- signIn.js
+++ signIn.js
@@ -5,15 +5,15 @@ let input = document.getElementById("input");
 
 button.addEventListener("click", ()=>{
     prevent();
-    sessionStorage.setItem("email", JSON.stringify(input.value));    
+        
 });
 
 function prevent(){
     if(input.value !== null || input.value !== ""){
-        console.log(input.value)
+        sessionStorage.setItem("email", JSON.stringify(input.value));
         window.location.href = "signUp.html";
         input.value="";       
-    }else{
+    }else if(input== null || input == ""){
         alert("Enter email address!")      
     }
 };
